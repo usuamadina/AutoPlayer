f2e4e8de3aa333143cf879b5c5b65fc36f341105
Usua Madina <u.madina.i@gmail.com>
2017-06-22 19:39:20 +0200

    Integrating the external playlist with the music service

--- app/src/main/java/com/example/automediabasic/MusicServiceBrowserTest.java
+++ app/src/main/java/com/example/automediabasic/MusicServiceBrowserTest.java
@@ -1,6 +1,5 @@
 package com.example.automediabasic;
 
-
 import android.media.MediaMetadata;
 import android.media.MediaPlayer;
 import android.media.browse.MediaBrowser;
@@ -15,6 +14,14 @@ import android.service.media.MediaBrowserService;
 import android.util.Log;
 
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,78 +32,31 @@ public class MusicServiceBrowserTest extends MediaBrowserService {
     private MediaPlayer mPlayer;
     private MediaMetadata mCurrentTrack;
 
+    private final String TAG = MusicServiceBrowserTest.this.getClass().getSimpleName();
+    private final String URL = "http://storage.googleapis.com/automotive-media/music.json";
+    private VolleySingleton volleySingleton;
+    private RequestQueue requestQueue;
+
+    private Gson gson;
+    private Music music;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        volleySingleton = VolleySingleton.getInstance(this.getApplicationContext());
+        requestQueue = volleySingleton.getRequestQueue();
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gson = gsonBuilder.create();
         mMusic = new ArrayList<MediaMetadata>();
-        //Añadimos 3 canciones desde la librería de audio de youtube
-        mMusic.add(new MediaMetadata.Builder().putString(MediaMetadata.METADATA_KEY_MEDIA_ID, "https://www.youtube.com/audiolibrary_download?vid=f5cfb6bd8c048b98").putString(MediaMetadata.METADATA_KEY_TITLE, "Primera canción").putString(MediaMetadata.METADATA_KEY_ARTIST, "Artista 1").putLong(MediaMetadata.METADATA_KEY_DURATION, 109000).build());
-        mMusic.add(new MediaMetadata.Builder().putString(MediaMetadata.METADATA_KEY_MEDIA_ID, "https://www.youtube.com/audiolibrary_download?vid=ac7a38f4a568229c").putString(MediaMetadata.METADATA_KEY_TITLE, "Segunda canción").putString(MediaMetadata.METADATA_KEY_ARTIST, "Artista 2").putLong(MediaMetadata.METADATA_KEY_DURATION, 65000).build());
-        mMusic.add(new MediaMetadata.Builder().putString(MediaMetadata.METADATA_KEY_MEDIA_ID, "https://www.youtube.com/audiolibrary_download?vid=456229530454affd").putString(MediaMetadata.METADATA_KEY_TITLE, "Tercera canción").putString(MediaMetadata.METADATA_KEY_ARTIST, "Artista 3").putLong(MediaMetadata.METADATA_KEY_DURATION, 121000).build());
         mPlayer = new MediaPlayer();
         mSession = new MediaSession(this, "MiServicioMusical");
-        mSession.setCallback(new MediaSession.Callback() {
-            @Override
-            public void onPlayFromMediaId(String mediaId, Bundle extras) {
-                for (MediaMetadata item : mMusic) {
-                    if (item.getDescription().getMediaId().equals(mediaId)) {
-                        mCurrentTrack = item;
-                        break;
-                    }
-                }
-                handlePlay();
-            }
+        getMusicalRepertoire();
 
-            @Override
-            public void onSkipToNext() {
-                super.onSkipToNext();
-                int currentIndex = mMusic.indexOf(mCurrentTrack);
-                Log.e("onSkipToNext()","click" + currentIndex);
-                if(currentIndex < mMusic.size()-1){
-                    currentIndex++;
-                }
-                else currentIndex = 0;
-                mCurrentTrack = mMusic.get(currentIndex);
-                mSession.setPlaybackState(buildState(PlaybackState.STATE_SKIPPING_TO_NEXT));
-                handlePlay();
-            }
 
-            @Override
-            public void onSkipToPrevious() {
-                super.onSkipToPrevious();
-                Log.e("onSkipToPrevious()","click");
-                int currentIndex = mMusic.indexOf(mCurrentTrack);
-                Log.e("onSkipToNext()","click" + currentIndex);
-                if(currentIndex ==0){
-                    currentIndex = mMusic.size()-1;
-                }
-                else currentIndex--;
-                mCurrentTrack = mMusic.get(currentIndex);
-                mSession.setPlaybackState(buildState(PlaybackState.STATE_SKIPPING_TO_NEXT));
-                handlePlay();
-
-            }
-
-            @Override
-            public void onPlay() {
-                if (mCurrentTrack == null) {
-                    mCurrentTrack = mMusic.get(0);
-                    handlePlay();
-                } else {
-                    mPlayer.start();
-                    mSession.setPlaybackState(buildState(PlaybackState.STATE_PLAYING));
-                }
-            }
-
-            @Override
-            public void onPause() {
-                mPlayer.pause();
-                mSession.setPlaybackState(buildState(PlaybackState.STATE_PAUSED));
-            }
-        });
+        mSession = new MediaSession(this, "MiServicioMusical");
         mSession.setFlags(MediaSession.FLAG_HANDLES_MEDIA_BUTTONS | MediaSession.FLAG_HANDLES_TRANSPORT_CONTROLS);
         mSession.setActive(true);
-        setSessionToken(mSession.getSessionToken());
+
     }
 
     private PlaybackState buildState(int state) {
@@ -132,6 +92,7 @@ public class MusicServiceBrowserTest extends MediaBrowserService {
         mPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mediaPlayer) {
+                mSession.setPlaybackState(buildState(PlaybackState.STATE_PLAYING));
                 mediaPlayer.start();
             }
         });
@@ -155,6 +116,115 @@ public class MusicServiceBrowserTest extends MediaBrowserService {
     @Override
     public void onDestroy() {
         mSession.release();
+    }
+
+
+    private void getMusicalRepertoire() {
+        StringRequest request = new StringRequest(Request.Method.GET, URL, onPostsLoaded, onPostsError);
+        requestQueue.add(request);
+        Log.e(TAG, "request = " + requestQueue);
+    }
+
+    private final Response.Listener<String> onPostsLoaded = new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+            music = gson.fromJson(response, Music.class);
+            Log.d(TAG, "Número de pistas de audio: " + music.getMusic().size());
+            int slashPos = URL.lastIndexOf('/');
+            String path = URL.substring(0, slashPos + 1);
+            for (int i = 0; i < music.getMusic().size(); i++) {
+                AudioTrack track = music.getMusic().get(i);
+                Log.e(TAG, "" + track.getTitle());
+
+                if (!track.getSource().startsWith("http"))
+                    track.setSource(path + track.getSource());
+                if (!track.getImage().startsWith("http")) track.setImage(path + track.getImage());
+
+                music.getMusic().set(i, track);
+                Log.e(TAG, "" + track.getTitle());
+                Log.e(TAG, "" + track.getSource());
+                Log.e(TAG, "" + track.getArtist());
+                Log.e(TAG, "" + track.getDuration());
+                mMusic.add(new MediaMetadata.Builder()
+                        .putString(MediaMetadata.METADATA_KEY_MEDIA_ID, track.getSource())
+                        .putString(MediaMetadata.METADATA_KEY_TITLE, track.getTitle())
+                        .putString(MediaMetadata.METADATA_KEY_ARTIST, track.getArtist())
+                        .putLong(MediaMetadata.METADATA_KEY_DURATION, track.getDuration() * 1000).build());
+
+            }
+
+            Log.e(TAG, "onResponse:Ya tengo la musica " + mMusic.size());
+            InitializeMediaSession();
+        }
+    };
+    private final Response.ErrorListener onPostsError = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.e(TAG, error.toString());
+        }
+    };
+
+
+    private void InitializeMediaSession() {
+        mSession.setCallback(new MediaSession.Callback() {
+            @Override
+            public void onPlayFromMediaId(String mediaId, Bundle extras) {
+                for (MediaMetadata item : mMusic) {
+                    if (item.getDescription().getMediaId().equals(mediaId)) {
+                        mCurrentTrack = item;
+                        break;
+                    }
+                }
+                handlePlay();
+            }
+
+            @Override
+            public void onSkipToNext() {
+                super.onSkipToNext();
+                int currentIndex = mMusic.indexOf(mCurrentTrack);
+                Log.e("onSkipToNext()", "click" + currentIndex);
+                if (currentIndex < mMusic.size() - 1) {
+                    currentIndex++;
+                } else currentIndex = 0;
+                mCurrentTrack = mMusic.get(currentIndex);
+                mSession.setPlaybackState(buildState(PlaybackState.STATE_SKIPPING_TO_NEXT));
+                handlePlay();
+            }
+
+            @Override
+            public void onSkipToPrevious() {
+                super.onSkipToPrevious();
+                Log.e("onSkipToPrevious()", "click");
+                int currentIndex = mMusic.indexOf(mCurrentTrack);
+                Log.e("onSkipToNext()", "click" + currentIndex);
+                if (currentIndex == 0) {
+                    currentIndex = mMusic.size() - 1;
+                } else currentIndex--;
+                mCurrentTrack = mMusic.get(currentIndex);
+                mSession.setPlaybackState(buildState(PlaybackState.STATE_SKIPPING_TO_NEXT));
+                handlePlay();
+
+            }
+
+            @Override
+            public void onPlay() {
+                if (mCurrentTrack == null) {
+                    mCurrentTrack = mMusic.get(0);
+                    handlePlay();
+                } else {
+                    mPlayer.start();
+                    mSession.setPlaybackState(buildState(PlaybackState.STATE_PLAYING));
+                }
+            }
+
+            @Override
+            public void onPause() {
+                mPlayer.pause();
+                mSession.setPlaybackState(buildState(PlaybackState.STATE_PAUSED));
+            }
+        });
+
+        setSessionToken(mSession.getSessionToken());
     }
 
 }
